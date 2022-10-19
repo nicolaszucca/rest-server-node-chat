898051caca8ef6a23436dd68414f3aba72d22f52
Nicolas Zuccarello <zuccarelo1@gmail.com>
2022-10-19 18:38:37 -0300

    add validations to the authenticated user

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,8 +1,10 @@
 const { request, response } = require('express');
 const jwt = require('jsonwebtoken');
 
+const Usuario = require('../models/user');
 
-const validarJWT = (req = request, res = response, next) => {
+
+const validarJWT = async (req = request, res = response, next) => {
 
     const token = req.header('x-token')
 
@@ -13,8 +15,23 @@ const validarJWT = (req = request, res = response, next) => {
 
     try {
 
-        jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const { id } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
 
+        const user = await Usuario.findById(id);
+
+        if (!user) {
+            return res.status(401).json({
+                msg: 'El usuario no existe en DB'
+            })
+        }
+
+        if (!user.estado) {
+            return res.status(401).json({
+                msg: 'Estado = false'
+            })
+        }
+
+        req.usuario = user
         next();
 
     } catch (error) {
