7a4e0c14861291995326371de01567bc311f24ba
Nicolas Zuccarello <zuccarelo1@gmail.com>
2023-05-16 23:35:14 -0300

    Added middleware to validate that the file exists

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validarCampos = require('../middlewares/validar-campos');
 const validarJWT = require('../middlewares/validar-jwt');
 const validarRol = require('../middlewares/validar-rol');
+const validarArchivo = require('../middlewares/validar-files')
 
 module.exports = {
     ...validarCampos,
     ...validarJWT,
-    ...validarRol
+    ...validarRol,
+    ...validarArchivo
 }
