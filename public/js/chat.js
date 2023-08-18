d46ffdbaed788c5fe90e2d15ab07f93f8ebadee6
Nicolas Zuccarello <zuccarelo1@gmail.com>
2023-08-18 17:02:34 -0300

    Add visual chat and functions to handle messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,6 +3,15 @@ const url = '/api/auth/'
 let user = null;
 let socket = null;
 
+
+
+//HTML REFERECES
+const txtUid = document.querySelector('#txtUid');
+const txtMensaje = document.querySelector('#txtMensaje');
+const ulUsuarios = document.querySelector('#ulUsuarios');
+const ulMensajes = document.querySelector('#ulMensajes');
+const btnSalir = document.querySelector('#btnSalir');
+
 const validarJWT = async () => {
 
     const tokenDB = localStorage.getItem('token') || '';
@@ -26,19 +35,83 @@ const validarJWT = async () => {
 };
 
 
+const conectarSocket = async () => {
+    socket = io({
+        extraHeaders: {
+            "x-token": localStorage.getItem('token')
+        }
+    });
+
+    socket.on('connect', () => {
+        console.log("Sockets online")
+    })
+    socket.on('disconnect', () => {
+        console.log("Sockets offline")
+    })
+    socket.on('recibir-mensajes', (payload) => {
+        console.log(payload)
+        mostrarMensajes(payload)
+    })
+    socket.on('usuarios-activos', (payload) => {
+        mostrarUsuarios(payload);
+    })
+    socket.on('mensaje-privado', (payload) => {
+        console.log(payload)
+    })
+
+
+}
+
+const mostrarUsuarios = (usuarios = []) => {
+    let usersHtml = '';
+    usuarios.forEach(({ nombre, _id }) => {
+
+        usersHtml += ` 
+        <li>
+            <p>
+                <h5 class="text-success"> ${nombre}</h5>
+                <span>${_id}</span>
+            </p>
+        </li>
+        `
+    })
+    ulUsuarios.innerHTML = usersHtml;
+}
+
+const mostrarMensajes = (mensajes = []) => {
+    let mensajesHtml = '';
+
+    mensajes.forEach(({ nombre, mensaje }) => {
+
+        mensajesHtml += ` 
+        <li>
+            <p>
+                <span class="text-primary">${nombre}:</span>
+                <span>${mensaje}</span>
+            </p>
+        </li>
+        `
+    })
+    ulMensajes.innerHTML = mensajesHtml;
+}
+
+
+txtMensaje.addEventListener('keyup', ({ keyCode }) => {
+
+    const mensaje = txtMensaje.value.trim();
+    const id = txtUid.value.trim();
+
+    if (keyCode != 13) { return }
+    if (mensaje.length === 0) { return }
+
+    socket.emit('enviar-mensaje', { mensaje, id })
+
+    txtMensaje.value = '';
+})
+
 const main = async () => {
 
     await validarJWT();
 
 }
 main();
-
-
-
-const conectarSocket = async () => {
-    const socket = io({
-        extraHeaders: {
-            "x-token": localStorage.getItem('token')
-        }
-    });;
-}
