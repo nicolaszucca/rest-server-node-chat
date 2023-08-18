df7155e6b2b93bc2311829f88ed50461f73d2097
Nicolas Zuccarello <zuccarelo1@gmail.com>
2023-08-18 17:06:19 -0300

    Add: backend sockets logic and events

--- sockets/controller.sockets.js
+++ sockets/controller.sockets.js
@@ -1,14 +1,38 @@
 const { comprobarJWT } = require("../middlewares/validar-jwt");
 
+const { ChatMensajes } = require("../models");
+const chatMensajes = new ChatMensajes()
 
-
-const socketController = async (socket) => {
+const socketController = async (socket, io) => {
     //Validar jwt 
     const usuario = await comprobarJWT(socket.handshake.headers['x-token']);
 
     if (!usuario) { return socket.disconnect() }
 
-    console.log(`${usuario.nombre} conectado`);
+    //agregar usuario existente conectado:
+    chatMensajes.agregarUsuario(usuario);
+
+    io.emit('usuarios-activos', chatMensajes.usuariosArr)
+
+    socket.join(usuario.id)
+
+    socket.on('disconnect', () => {
+        chatMensajes.desconectarUsuario(usuario.id)
+        io.emit('usuarios-activos', chatMensajes.usuariosArr)
+    })
+
+    socket.on('enviar-mensaje', ({ mensaje, id }) => {
+
+        if (id) {
+
+            socket.to(id).emit('mensaje-privado', { de: usuario.nombre, mensaje: mensaje })
+
+        } else {
+
+            chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje);
+            io.emit('recibir-mensajes', chatMensajes.ultimos10)
+        }
+    })
 }
 
 module.exports = {
